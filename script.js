326f11c2b50cd4924b058d893ae551d57c9ae142
Parachute-alt <elody@studioparachute.co>
2025-04-20 19:26:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 
 function calculerROI() {
-    // Récupérer les valeurs des champs
     let investissement = parseFloat(document.getElementById('investissement').value);
     let revenus = parseFloat(document.getElementById('revenus').value);
     
-    // Vérifier si les entrées sont valides
     if (isNaN(investissement) || isNaN(revenus) || investissement <= 0 || revenus <= 0) {
         alert("Veuillez entrer des valeurs valides pour l'investissement et les revenus.");
         return;
     }
-    
-    // Calculer le ROI
+
     let roi = ((revenus - investissement) / investissement) * 100;
     let message = `<div><strong>Votre ROI est de ${roi.toFixed(2)} %</strong></div>`;
     let explanation = "";
@@ -23,6 +20,5 @@ function calculerROI() {
         explanation = `<div class="explication">Cela signifie que votre investissement n’a pas encore généré de retour financier. Il peut être pertinent de retravailler votre positionnement de marque, votre offre ou encore votre compréhension de la demande client. <strong>Malgré ce résultat vous avez aussi renforcé votre visibilité, amélioré la reconnaissance de votre marque et ouvert la porte à de futures opportunités commerciales.</strong></div>`;
     }
 
-    // Affichage structuré dans le div de résultat
     document.getElementById('resultat').innerHTML = message + explanation;
 }
