6d37c8cc9c5536daedeb2e944dd1b790befac070
Parachute-alt <elody@studioparachute.co>
2025-04-20 16:45:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ function calculerROI() {
     let message;
 
     if (roi > 0) {
-        message = `Votre ROI est de ${roi.toFixed(2)} %. Cela signifie que votre investissement a été rentable.`;
+        message = `Votre ROI est de ${roi.toFixed(2)} %. Cela signifie que votre investissement a été rentable : non seulement vous avez récupéré votre mise, mais vous avez également généré des bénéfices. <strong>Cela reflète un bon alignement entre votre offre et les attentes de votre clientèle cible.</strong> En plus du retour immédiat, vous avez aussi renforcé votre visibilité, amélioré la reconnaissance de votre marque et ouvert la porte à de futures opportunités commerciales.`;
     } else if (roi === 0) {
         message = "Votre ROI est de 0 %. Vous avez récupéré exactement ce que vous avez investi.";
     } else {
-        message = `Votre ROI est de ${roi.toFixed(2)} %. Cela signifie que vous avez perdu de l'argent sur cet investissement.`;
+        message = `Votre ROI est de ${roi.toFixed(2)} %. Cela signifie que votre investissement n’a pas encore généré de retour financier. Il peut être pertinent de retravailler votre positionnement de marque, votre offre ou encore votre compréhension de la demande client. <strong>Malgré ce résultat vous avez aussi renforcé votre visibilité, amélioré la reconnaissance de votre marque et ouvert la porte à de futures opportunités commerciales.</strong>`;
     }
 
-    // Afficher le résultat
-    document.getElementById('resultat').textContent = message;
+    // Afficher le résultat avec HTML interprété
+    document.getElementById('resultat').innerHTML = message;
 }
